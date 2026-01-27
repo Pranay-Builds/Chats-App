79263c4d0cc4ebc74e6ed8f867e8901fe31167b6
Pranay <contactpranay10@gmail.com>
2026-01-27 18:03:49 +0530

    (setup) react project (feat) make landing page and navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between px-6 py-4 sticky top-0 z-50">
+
+            <div className="flex items-center">
+                <img src="/icon.png" className="w-10 h-10" />
+                <span className="ml-2 text-xl font-semibold">Chats</span>
+            </div>
+
+
+            <div className="flex items-center gap-4">
+                <button className="text-sm text-[#9ba3b4] hover:text-white transition">
+                    Log in
+                </button>
+                <button className="text-sm px-4 py-2 rounded-lg bg-white text-black font-medium hover:bg-gray-200 transition">
+                    Sign up
+                </button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
